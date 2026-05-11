d3c10d197e91408f68bab3e3a1c758d9b0e6edc7
ggoranii <lsgustj@gmail.com>
2026-05-11 15:35:39 +0900

    Update hashtable_impl.cpp

--- lab2/hashtable_impl.cpp
+++ lab2/hashtable_impl.cpp
@@ -9,6 +9,7 @@
 
 #include "hashtable_impl.h"
 #include <cstdlib>
+#include <pthread.h>
 #include <vector>
 #include <algorithm>
 
@@ -163,19 +164,98 @@ CoarseHashTable::~CoarseHashTable() {
 }
 
 void CoarseHashTable::insert(int key, int value) {
-    // 구현
+    
+    pthread_mutex_lock(&mutex_lock);        // 락
+
+    // 해시 함수로 버킷 인덱스 구함
+    int index = hash_func(key);
+
+    // 연결 리스트 첫번째 노드 가리킴
+    HTNode* curr = buckets_[index];
+
+    // 연결 리스트에 동일 키 존재하는지 확인
+    while (curr != nullptr) {
+        // 이미 키가 존재하면 value, upd_cnt 증가
+        if (curr->key == key) {
+            curr->value += value;
+            curr->upd_cnt++;
+            pthread_mutex_unlock(&mutex_lock);      // 언락
+            return;            
+        }
+        curr = curr->next;
+    }
+
+    // 동일한 키가 없는 경우 새 노드 생성
+    HTNode* new_node = new HTNode;
+    new_node->key = key;
+    new_node->upd_cnt = 0;
+    new_node->value = value;
+
+    // 새 노드를 연결 리스트 맨 앞으로
+    new_node->next = buckets_[index];   // 기존 첫 노드를 새 노드 다음으로 포인터 설정
+    buckets_[index] = new_node;         // 버킷 시작점을 새 노드로 설정
+
+    pthread_mutex_unlock(&mutex_lock);  // 언락
 }
 
 int CoarseHashTable::lookup(int key) {
-    // 구현
+    
+    pthread_mutex_lock(&mutex_lock);    // 락
+
+    // 해시 함수로 버킷 인덱스 구함
+    int index = hash_func(key);
+
+    // 버킷 인덱스로 탐색
+    HTNode* curr = buckets_[index];
+
+    while (curr != nullptr) {
+     // key 값 일치하면 value 반환
+        if (curr->key == key)  {
+            int val = curr->value;
+            pthread_mutex_unlock(&mutex_lock);     // 언락
+            return val;
+        }
+        // 다음 노드로 이동 
+        curr = curr->next;
+    }
+     pthread_mutex_unlock(&mutex_lock);            // 언락
+     return 0;   // value가 없으면 0 반환
 }
 
 void CoarseHashTable::remove(int key) {
-    // 구현
+    
+    pthread_mutex_lock(&mutex_lock);    // 락
+
+    int index = hash_func(key);
+    HTNode* curr = buckets_[index];
+    HTNode* prev = nullptr;             // 맨 앞 노드는 이전 노드 존재 x -> nullptr
+
+    while (curr != nullptr) {
+        if (curr->key == key) {
+            if (prev == nullptr) {
+                // 첫 노드를 삭제하는 경우
+                buckets_[index] = curr->next;
+            } else {
+            // 중간 노드 삭제
+            prev->next = curr->next;
+            }
+        // 메모리 누수 방지 위해 메모리 해제
+        delete curr;
+        pthread_mutex_unlock(&mutex_lock);      // 언락
+        return;
+        }
+    // 다음 노드로 이동
+    prev = curr;
+    curr = curr->next;
+    }
+    pthread_mutex_unlock(&mutex_lock);          // 언락
 }
 
 void CoarseHashTable::traversal(KVC* arr) {
-    // 구현
+    pthread_mutex_lock(&mutex_lock);            // 락
+    // traversal 함수 호출
+    DefaultHashTable::traversal(arr);           
+    pthread_mutex_unlock(&mutex_lock);          // 언락
 }
 
 // FineHashTable (fine-grained lock)
